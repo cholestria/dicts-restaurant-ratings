d9ea6ae0c6e964b287db0af5afe4285139465275
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-09 16:11:47 -0800

    added user inputs

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -5,12 +5,19 @@ def rating_restaurants(filename):
 
     restaurant_ratings = {}
 
+    restaurant_name = raw_input("What's the restaurant's name?")
+    restaurant_score = int(raw_input("What's the restaurant's score?"))
+
     for line in open_file:
         line = line.rstrip()
         restaurant, rating = line.split(':')
         restaurant_ratings[restaurant] = rating
 
+    restaurant_ratings[restaurant_name] = restaurant_score    
+
     for restaurant, rating in sorted(restaurant_ratings.items()):
             print "%s is rated %s" % (restaurant, rating)
+
+    open_file.close()
 
 rating_restaurants("scores.txt")
