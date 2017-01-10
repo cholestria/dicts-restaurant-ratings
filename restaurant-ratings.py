87e2876f2d7be4ee787efd43a02eaa781857a51c
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-09 16:28:40 -0800

    working resturant rating with some formatting errors

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,3 +1,38 @@
+def user_chooses(filename):
+    """Asks a user what they want to do """
+
+    print "What would you like to do? \
+            1. See all the ratings in alphabetical order. \
+            2. Add a new resturant and rating. \
+            3. Quit."
+
+    user_choice = raw_input("Select 1, 2, or 3: ")
+    if user_choice == "1":
+        just_print(filename)
+    elif user_choice == "2":
+        rating_restaurants(filename)
+    else:
+        exit()
+
+
+def just_print(filename):
+    """print list without input"""
+
+    open_file = open(filename)
+
+    restaurant_ratings = {}
+
+    for line in open_file:
+        line = line.rstrip()
+        restaurant, rating = line.split(':')
+        restaurant_ratings[restaurant] = rating
+
+    for restaurant, rating in sorted(restaurant_ratings.items()):
+        print "%s is rated %s" % (restaurant, rating)
+
+    open_file.close()
+
+
 def rating_restaurants(filename):
     """Sorts rated restaurants in alphabetical order"""
 
@@ -20,4 +55,5 @@ def rating_restaurants(filename):
 
     open_file.close()
 
-rating_restaurants("scores.txt")
+#rating_restaurants("scores.txt")
+user_chooses("scores.txt")        
